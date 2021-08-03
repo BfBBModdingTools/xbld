f888ae5deae610745c5801e36ea9db41026da502
Steven Casper <sebastiancasper3@gmail.com>
2021-08-03 03:42:04 -0400

    tests: remove hardcoded default hash

--- src/xbe/mod.rs
+++ src/xbe/mod.rs
@@ -459,23 +459,23 @@ mod tests {
     fn vanilla_serialization() -> Result<(), std::io::Error> {
         use sha1::{Digest, Sha1};
 
+        let default_bytes = std::fs::read("test/bin/default.xbe")?;
+        let default_hash = {
+            let mut hasher = Sha1::new();
+            hasher.update(&default_bytes);
+            hasher.finalize()
+        };
+
+        // TODO: use a constructor that takes the byte array
         let xbe = Xbe::from_path("test/bin/default.xbe");
         let bytes = xbe.serialize()?;
-
-        // ensure the files are the same.
-        // TODO: implement this as a function in the XBE module/crate and update chum_bucket_lab
-        // to use it instead (and this test)
-        const XBE_SHA1: &'static [u8] = &[
-            0xa9, 0xac, 0x85, 0x5c, 0x4e, 0xe8, 0xb4, 0x1b, 0x66, 0x1c, 0x35, 0x78, 0xc9, 0x59,
-            0xc0, 0x24, 0xf1, 0x06, 0x8c, 0x47,
-        ];
-        let hash = {
+        let output_hash = {
             let mut hasher = Sha1::new();
             hasher.update(&bytes);
             hasher.finalize()
         };
 
-        assert_eq!(*XBE_SHA1, *hash);
+        assert_eq!(*default_hash, *output_hash);
         Ok(())
     }
 }
