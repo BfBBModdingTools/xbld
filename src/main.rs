0c580d22099d28a485009143fbdb93cd1e6e5cc7
Steven Casper <sebastiancasper3@gmail.com>
2021-09-27 19:43:51 -0400

    Rewrite CLI with clap

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,62 @@
-use bfbb_linker::{
-    config::Configuration,
-    error::{CliError, Error, Result},
-    xbe,
-};
+use bfbb_linker::{config::Configuration, error::Error, xbe};
+use clap::{App, Arg};
 use std::{env, process};
 
-struct Cli<'a> {
-    config: Configuration<'a>,
+struct Cli {
+    config: String,
     input_path: String,
     output_path: String,
 }
 
 fn main() {
-    let cli = match parse_args(env::args()) {
-        Ok(c) => c,
-        Err(e @ Error::Cli(CliError::HelpRequested)) => {
-            println!("{}", e);
-            process::exit(0)
-        }
-        Err(e) => {
-            eprintln!("{}", e);
-            process::exit(1)
-        }
-    };
+    let cli = parse_args();
 
-    if let Err(e) = do_injection(cli) {
+    if let Err(e) = do_injection(&cli) {
         eprintln!("{}", e);
         process::exit(1);
     }
 }
 
-fn do_injection(cli: Cli) -> std::result::Result<(), Box<dyn std::error::Error>> {
+fn do_injection(cli: &Cli) -> Result<(), Box<dyn std::error::Error>> {
+    let config = Configuration::from_toml(
+        std::fs::read_to_string(&cli.config)
+            .map_err(|e| Error::Io(cli.config.clone(), e))?
+            .as_str(),
+    )?;
     let xbe: xbe::Xbe =
-        bfbb_linker::inject(cli.config, xbe::Xbe::new(&std::fs::read(cli.input_path)?)?)?;
-    std::fs::write(cli.output_path, xbe.serialize()?)?;
+        bfbb_linker::inject(config, xbe::Xbe::new(&std::fs::read(&cli.input_path)?)?)?;
+    std::fs::write(&cli.output_path, xbe.serialize()?)?;
 
     Ok(())
 }
 
-fn parse_args<'a, I>(mut args: I) -> Result<Cli<'a>>
-where
-    I: Iterator<Item = std::string::String>,
-{
-    // TODO: Implment switch alternatives for flags
-    const FLAG_HELP: &str = "--help";
-    const FLAG_CONFIG: &str = "--config";
-    const FLAG_INPUT_XBE: &str = "--input";
+fn parse_args() -> Cli {
+    let matches = App::new("BfBB Linker")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("A linker for patching and injecting cutom code into an XBE binary.")
+        .arg(
+            Arg::with_name("CONFIG")
+                .help("Config file specifying code to be injected")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("INPUT")
+                .help("XBE Binary to inject into")
+                .required(true)
+                .index(2),
+        )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .help("File path to write output to")
+                .required(true)
+                .index(3),
+        )
+        .get_matches();
 
-    let mut config: Option<String> = None;
-    let mut input_xbe: Option<String> = None;
-    let mut output_xbe: Option<String> = None;
-
-    // Skip over this program's name
-    args.next();
-    while let Some(next) = args.next() {
-        match next.as_str() {
-            FLAG_CONFIG => config = Some(args.next().unwrap()),
-            FLAG_INPUT_XBE => input_xbe = Some(args.next().unwrap()),
-            FLAG_HELP => return Err(Error::Cli(CliError::HelpRequested)),
-            s => output_xbe = Some(s.to_owned()),
-        }
-    }
-
-    // Unwrap parameters
-    let config = config.ok_or(Error::Cli(CliError::MissingArgument(
-        "Config file is required.",
-    )))?;
-    let config = std::fs::read_to_string(config.as_str()).map_err(|e| Error::Io(config, e))?;
-
-    let input_path = input_xbe.ok_or(Error::Cli(CliError::MissingArgument(
-        "Input XBE is required.",
-    )))?;
-    let output_path = output_xbe.ok_or(Error::Cli(CliError::MissingArgument(
-        "Output XBE is required",
-    )))?;
-
-    let config = Configuration::from_toml(config.as_str())?;
-    Ok(Cli {
-        config,
-        input_path,
-        output_path,
-    })
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn cli() {
-        let args = vec![
-            "program_name".to_string(),
-            "--config".to_string(),
-            "test/bin/conf.toml".to_string(),
-            "--input".to_string(),
-            "test/bin/default.xbe".to_string(),
-            "bin/output.xbe".to_string(),
-        ];
-        let c = parse_args(args.into_iter());
-
-        assert!(c.is_ok());
+    Cli {
+        config: matches.value_of("CONFIG").unwrap().to_string(),
+        input_path: matches.value_of("INPUT").unwrap().to_string(),
+        output_path: matches.value_of("OUTPUT").unwrap().to_string(),
     }
 }
